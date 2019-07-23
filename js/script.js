b6eb4360fb4fe1c3d660f7e79a82d733bc250e23
Amber Metz <ambermetz88@gmail.com>
2019-07-23 15:13:24 -0400

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,52 @@
+const randomDamage = () => Math.floor(Math.random() * 10 + 1);
+
+const chooseOption = (opt1, opt2) => {
+  const randNum = Math.round(Math.random());
+  const ranNum = 0 ? opt1 : opt2;
+};
+
+function attackPlayer(health) {
+  return health - randomDamage;
+}
+
+const logHealth = (player, health) =>
+  console.log(`${player} health: ${health}`);
+
+const logDeath = (winner, loser) => console.log(`${winner} defeated ${loser}`);
+
+const isDead = health => {
+  if (health <= 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const fight = function() {
+  let player1 = "John Connor";
+  let player2 = "The Terminator";
+  let player1Health = 100;
+  let player2Health = 100;
+
+  while (true) {
+    let attacker = chooseOption(player1, player2);
+
+    if (attacker === player1) {
+      player2Health = attackPlayer(player2Health);
+      logHealth(player2, player2Health);
+      if (isDead(player2Health)) {
+        logDeath(player1, player2);
+        break;
+      } else {
+        player1Health = attackPlayer(player1Health);
+        logHealth(player1, player1Health);
+        if (isDead(player1Health)) {
+          logDeath(player2, player1);
+          break;
+        }
+      }
+    }
+  }
+};
+
+fight();
